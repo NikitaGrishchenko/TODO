ef50e406d91c625e0a56abc2592691ab29e6e5f9
Grishchenko <grishchenko@rsue.ru>
2020-11-10 17:39:00 +0300

    add page login

--- backend/apps/todo/v1/views.py
+++ backend/apps/todo/v1/views.py
@@ -1,20 +1,28 @@
 from rest_framework import generics
 from ..models import Todo
 from .serializers import TodoListSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
+from coreapi.auth import BasicAuthentication, SessionAuthentication
+from django.contrib.auth.decorators import login_required
 
 
 class TodoListView(generics.ListCreateAPIView):
     """ Вывод списка задач """
 
-    permission_classes = [AllowAny]
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = TodoListSerializer
     queryset = Todo.objects.all()
+
+    @login_required
+    def my_view(request):
+        pass
 
 
 class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
     """ Обновление, удаление, детальная информация задачи """
 
-    permission_classes = [AllowAny]
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = TodoListSerializer
     queryset = Todo.objects.all()
