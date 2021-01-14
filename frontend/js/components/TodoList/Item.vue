d83ef5f75efcdbdd2035ccc4c9d4255e5c0923cb
NikitaGrishchenko <grishchenkonikita@mail.ru>
2021-01-14 23:06:10 +0300

    fix bug for checkbox

--- frontend/js/components/TodoList/Item.vue
+++ frontend/js/components/TodoList/Item.vue
@@ -33,14 +33,14 @@
           {{ format_date }}
         </div>
       </div>
-      <div class="todo-left d-flex justify-content-between align-items-center">
-        <div class="todo-item__change">
+      <div class="todo-left">
+        <!-- <div class="todo-item__change">
           <img
             src="static/img/pencil.png"
             alt="pencil"
             class="todo-item__pencil"
           />
-        </div>
+        </div> -->
         <div>
           <div class="todo-item__remove" @click="remove">
             <img
